a34f1dbc1115edcd72d62b9d8a44e078895fc050
sxmtee <somtoo76@gmail.com>
2024-12-13 14:40:54 +0100

    Snowseeker Project Done

--- SnowSeeker/Screens/ResortView.swift
+++ SnowSeeker/Screens/ResortView.swift
@@ -70,9 +70,17 @@ struct ResortView: View {
     var body: some View {
         ScrollView {
             VStack(alignment: .leading, spacing: 0) {
-                Image(decorative: resort.id)
-                    .resizable()
-                    .scaledToFit()
+                ZStack(alignment: .bottomTrailing) {
+                    Image(decorative: resort.id)
+                        .resizable()
+                        .scaledToFit()
+                    
+                    Text(resort.imageCredit)
+                        .padding(10)
+                        .background(.black.opacity(0.7))
+                        .foregroundStyle(.white)
+                        .offset(x: -5, y: -5)
+                }
                 
                 createDetailsView()
                 
